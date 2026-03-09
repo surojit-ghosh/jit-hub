306c39dcdf498dcb04a14e2d36e815963c21bd28
surojitghosh <contact@surojit.in>
2026-03-09 12:01:20 +0530

    add gitignore

---
+++ REAEDME.md
@@ -0,0 +1,3 @@
+<h3 align="center">Jit Hub</h3>
+
+> Jit Hub is a multi platform application for my personal usecases.
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.surojit.jithub',
+  appName: 'Jit Hub',
+  webDir: 'dist'
+};
+
+export default config;
